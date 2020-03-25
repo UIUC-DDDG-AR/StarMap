81801d782e8f40aef3619ea2dd0da7d8627875e8
mingir2 <mingir2@illinois.edu>
2020-03-24 19:14:48 -0500

    Create capability page

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -3,13 +3,14 @@ import './page.css'
 
 import GridView from '../gridView/gridView'
 import LandingPage from '../landingPage/landingPage'
+import CapabilityPage from '../capabilityPage/capabilityPage'
 
 const RenderPage = (props) => {
     const pageType = props.pageType;
     if(pageType === "interactive"){
         return null
     } else if (pageType === "capability"){
-        return <LandingPage />
+        return <CapabilityPage />
     } else if (pageType === "tools") {
         return <GridView toolType={"Hardware"}/>
     } else {
