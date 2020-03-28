82c7af8d70e692b632042a49f15f1a5f007f15c8
mingir2 <mingir2@illinois.edu>
2020-03-27 21:41:05 -0500

    Add data fomr CapabilityPage

--- src/pages/capabilityPage.js
+++ src/pages/capabilityPage.js
@@ -1,28 +1,8 @@
 import React, { Fragment } from "react";
 
 import Card from "../components/card";
+import CapabilityData from "../data/capability"
 import Logo from "../logo.svg";
-
-const pageData = [
-    {
-        title: "Tracking & Recognition",
-        img: Logo,
-        text:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis orci a scelerisque purus semper. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Eu tincidunt tortor aliquam nulla. Pharetra magna ac placerat vestibulum lectus. Sagittis aliquam malesuada bibendum arcu."
-    },
-    {
-        title: "Displaying Information",
-        img: Logo,
-        text:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis orci a scelerisque purus semper. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Eu tincidunt tortor aliquam nulla. Pharetra magna ac placerat vestibulum lectus. Sagittis aliquam malesuada bibendum arcu."
-    },
-    {
-        title: "Augmentation & Interaction",
-        img: Logo,
-        text:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis orci a scelerisque purus semper. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Eu tincidunt tortor aliquam nulla. Pharetra magna ac placerat vestibulum lectus. Sagittis aliquam malesuada bibendum arcu."
-    }
-];
 
 const CapabilityPage = () => {
     return (
@@ -31,8 +11,8 @@ const CapabilityPage = () => {
                 <h1>AR Capability</h1>
             </div>
             <div className="body">
-                {pageData.map(data => (
-                    <Card orient="horizontal" title={data.title} img={data.img}>
+                {CapabilityData.map(data => (
+                    <Card orient="horizontal" title={data.title} img={Logo}>
                         {data.text}
                     </Card>
                 ))}
