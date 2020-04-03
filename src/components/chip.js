258586520fa4fdc046094c37feeb9d69f991236d
mingir2 <mingir2@illinois.edu>
2020-04-03 00:17:40 -0500

    Add Chip component and update the toolsPage

---
+++ src/components/chip.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./chip.css";
+
+const Chip = props => {
+    const handleClick = event => {
+        event.stopPropagation();
+        props.onDelete()
+    };
+
+    return (
+        <span className={`orange-chip ${props.state}`} onClick={props.onClick}>
+            {props.children}
+            {props.state === "active" ? <svg height="20" width="28">
+                <g
+                    className="orange-chip-svg"
+                    fill="black"
+                    onClick={handleClick}
+                >
+                    <circle cx="60%" cy="50%" r="10" />
+                    <text
+                        x="60%"
+                        y="50%"
+                        textAnchor="middle"
+                        fill="white"
+                        strokeWidth="1"
+                        dy=".3em"
+                        fontSize="11px"
+                    >
+                        ✕
+                    </text>
+                </g>
+            </svg> : null}
+        </span>
+    );
+};
+export default Chip;
