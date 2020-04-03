103d6906756c803ee526839496fe8618ac752cb3
mingir2 <mingir2@illinois.edu>
2020-04-03 11:03:12 -0500

    Bind accordion active:after

--- src/components/chip.js
+++ src/components/chip.js
@@ -4,32 +4,34 @@ import "./chip.css";
 const Chip = props => {
     const handleClick = event => {
         event.stopPropagation();
-        props.onDelete()
+        props.onDelete();
     };
 
     return (
         <span className={`orange-chip ${props.state}`} onClick={props.onClick}>
             {props.children}
-            {props.state === "active" ? <svg height="20" width="28">
-                <g
-                    className="orange-chip-svg"
-                    fill="black"
-                    onClick={handleClick}
-                >
-                    <circle cx="60%" cy="50%" r="10" />
-                    <text
-                        x="60%"
-                        y="50%"
-                        textAnchor="middle"
-                        fill="white"
-                        strokeWidth="1"
-                        dy=".27em"
-                        fontSize="20px"
+            {props.state === "active" ? (
+                <svg height="20" width="28">
+                    <g
+                        className="orange-chip-svg"
+                        fill="black"
+                        onClick={handleClick}
                     >
-                        &times;
-                    </text>
-                </g>
-            </svg> : null}
+                        <circle cx="60%" cy="50%" r="10" />
+                        <text
+                            x="60%"
+                            y="50%"
+                            textAnchor="middle"
+                            fill="white"
+                            strokeWidth="1"
+                            dy=".27em"
+                            fontSize="20px"
+                        >
+                            &times;
+                        </text>
+                    </g>
+                </svg>
+            ) : null}
         </span>
     );
 };
