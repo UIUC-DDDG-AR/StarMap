9a575e3924d565e115f83c2502e64d4be162324c
mingir2 <mingir2@illinois.edu>
2020-04-05 15:16:19 -0500

    Integrate material ui and add capability info json

--- src/pages/capabilityPage.js
+++ src/pages/capabilityPage.js
@@ -1,36 +1,75 @@
-import React, { Fragment } from "react";
+import React from "react";
 
-import Card from "../components/card";
-import CapabilityData from "../data/capability"
-import Logo from "../logo.svg";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+
+import capability_information from "../data/capability_information";
+
+const useStyles = makeStyles({
+    header: {
+        padding: "3em 10vw",
+        background: "#011140",
+        position: "relative",
+        color: "white",
+    },
+    container: {
+        padding: "1em 5vw",
+    },
+    grid: {
+        flexGrow: 1,
+    },
+    card: {
+        boxShadow: "none",
+    },
+    title: {
+        //padding: "1em",
+    },
+    media: {
+        height: 300,
+    },
+    content: {
+        fontSize: 14,
+        padding: "1em 0",
+    },
+});
 
 const CapabilityPage = () => {
-    var arr = [];
+    const classes = useStyles();
+
     return (
-        <Fragment>
-            <div>
-                    {Object.keys(CapabilityData).forEach(function(element) {
-                            console.log(element)
-                            arr.push(CapabilityData[element])
-                    })}
-                </div>    
-
-
-            <div className="header">
+        <React.Fragment>
+            <div className={classes.header}>
                 <h1>Augmented Reality Capabilities</h1>
             </div>
-            <div className="body">
-                {arr.map((element, index, arrayobj) => {
-                    return(
-                        element.map((data, index, arrayobj) => (
-                            <Card key ={index} orient="horizontal" title={data.title} img={Logo}>
-                                {data.text}
-                            </Card>
-                        ))
-                    )
-                })}
-            </div>
-        </Fragment>
+
+            <Container className={classes.container}>
+                <Grid container spacing={3}>
+                    {capability_information.map((capability) => {
+                        return (
+                            <Grid item xs={6}>
+                                <Card className={classes.card}>
+                                    <h2 className={classes.title}>
+                                        {capability.name}
+                                    </h2>
+
+                                    <CardMedia
+                                        className={classes.media}
+                                        image="/images/Capability.png"
+                                    />
+                                    <CardContent className={classes.content}>
+                                        {capability.Descripton}
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Container>
+        </React.Fragment>
     );
 };
 export default CapabilityPage;
