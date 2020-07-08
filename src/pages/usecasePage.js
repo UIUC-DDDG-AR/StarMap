a885e1ace8c4ddf9387ae47322bfbbe791407b5c
Mingi Ryu <mingir2@illinois.edu>
2020-07-08 17:41:22 -0500

    Add UsecasePage
    Add header and content component

---
+++ src/pages/usecasePage.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography"
+
+const UsecasePageHeader = () => {
+    return (
+        <div></div>
+    )
+}
+
+
+const UsecasePageContent = () => {
+    return (
+        <div></div>
+    )
+}
+
+
+const UsecasePage = () => {
+    return (
+        <Fragment>
+            <UsecasePageHeader />
+            <UsecasePageContent />
+        </Fragment>
+    )
+}
+
+export default UsecasePage
