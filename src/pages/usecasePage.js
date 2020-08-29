323ea51c2bde8c8a38ab4ad58dc80547f34441ea
mingir2 <40753283+mingir2@users.noreply.github.com>
2020-08-29 11:40:41 -0500

    Automatic code formatting

--- src/pages/usecasePage.js
+++ src/pages/usecasePage.js
@@ -25,41 +25,41 @@ import card_content from "../data/example_page_content.json"
 
 const createData = (capability_id, capability_name, snapchat, pokemongo) => {
     return {
-      capability_id,
-      capability_name,
-      snapchat,
-      pokemongo,
-      content: card_content
+        capability_id,
+        capability_name,
+        snapchat,
+        pokemongo,
+        content: card_content
     };
 }
 
 const idFindName = (id, capability_information) => {
-  return capability_information.map(info => {
-    if (id === info.id) {
-      return info.name;
-    }
-  })
+    return capability_information.map(info => {
+        if (id === info.id) {
+            return info.name;
+        }
+    })
 }
 
 const data = [];
 
 usecase.map((cap) => {
     data.push(createData(
-      cap.id,
-      idFindName(cap.id, capability_information),
-      cap.Snapchat[0] === 1? 'X' : '', 
-      cap.pokmon_go[0] === 1? 'X' : ''))
+        cap.id,
+        idFindName(cap.id, capability_information),
+        cap.Snapchat[0] === 1 ? 'X' : '',
+        cap.pokmon_go[0] === 1 ? 'X' : ''))
 });
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.common.white,
-      fontWeight: "400",
-      textAlign: "center",
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+        fontWeight: "400",
+        textAlign: "center",
     },
     body: {
-      textAlign: "center",
+        textAlign: "center",
     },
 }))(TableCell);
 
@@ -68,24 +68,24 @@ const Row = (props) => {
         rows: {
             '&:nth-of-type(even)': {
                 backgroundColor: theme.palette.common.white,
-              },
-              '& > *': {
+            },
+            '& > *': {
                 borderRight: '0px',
             },
         },
         headText: {
-          fontWeight: 'bold'
+            fontWeight: 'bold'
         },
         card: {
-          boxShadow: "none",
+            boxShadow: "none",
         },
         media: {
-          height: 250,
+            height: 250,
         },
         content: {
-          fontSize: 16,
-          padding: "1em 0",
-          lineHeight: "1.25",
+            fontSize: 16,
+            padding: "1em 0",
+            lineHeight: "1.25",
         },
     }))()
 
@@ -94,114 +94,116 @@ const Row = (props) => {
 
     let snapCard;
     let pokemonCard;
-    
+
     if (row.snapchat === 'X') {
-      snapCard = 
-        row.content
-          .filter((cnt) => cnt.capability_id === row.capability_id)
-          .map((cnt) => (
-        <TableCell width="30%">
-          <Card className={classes.card}>
-              <CardMedia 
-                className={classes.media} 
-                component="img" 
-                alt={cnt.capability_id}
-                image={`/images/snapchat/${cnt.capability_id}.png`}
-              />
-              <CardContent className={classes.content}>
-                  <Typography variant="p">{cnt.snapchat}</Typography>
-              </CardContent>
-          </Card>
-        </TableCell>
-    ))}else {
-      snapCard = <TableCell width="30%"></TableCell>
+        snapCard =
+            row.content
+                .filter((cnt) => cnt.capability_id === row.capability_id)
+                .map((cnt) => (
+                    <TableCell width="30%">
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                component="img"
+                                alt={cnt.capability_id}
+                                image={`/images/snapchat/${cnt.capability_id}.png`}
+                            />
+                            <CardContent className={classes.content}>
+                                <Typography variant="p">{cnt.snapchat}</Typography>
+                            </CardContent>
+                        </Card>
+                    </TableCell>
+                ))
+    } else {
+        snapCard = <TableCell width="30%"></TableCell>
     }
 
     if (row.pokemongo === 'X') {
-      pokemonCard = 
-        row.content
-          .filter((cnt) => cnt.capability_id === row.capability_id)
-          .map((cnt) => (
-          <TableCell width="30%">
-            <Card className={classes.card}>
-                <CardMedia 
-                  className={classes.media} 
-                  component="img" 
-                  alt={cnt.capability_id}
-                  image={`/images/pokemon/${cnt.capability_id}.jpg`}
-                />
-                <CardContent className={classes.content}>
-                    <Typography variant="p">{cnt.pokemon_go}</Typography>
-                </CardContent>
-            </Card>
-          </TableCell>
-    ))}else {
-      pokemonCard = <TableCell width="30%"></TableCell>
+        pokemonCard =
+            row.content
+                .filter((cnt) => cnt.capability_id === row.capability_id)
+                .map((cnt) => (
+                    <TableCell width="30%">
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                component="img"
+                                alt={cnt.capability_id}
+                                image={`/images/pokemon/${cnt.capability_id}.jpg`}
+                            />
+                            <CardContent className={classes.content}>
+                                <Typography variant="p">{cnt.pokemon_go}</Typography>
+                            </CardContent>
+                        </Card>
+                    </TableCell>
+                ))
+    } else {
+        pokemonCard = <TableCell width="30%"></TableCell>
     }
-  
+
     return (
-      <React.Fragment>
-        <TableRow className={classes.rows}>
-          <TableCell  align="right" width="1%">
-            <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-          <StyledTableCell className={classes.headText} width="25%">{row.capability_name}</StyledTableCell>
-          <StyledTableCell >{row.snapchat}</StyledTableCell>
-          <StyledTableCell >{row.pokemongo}</StyledTableCell>
-        </TableRow>
-        <TableRow className={classes.rows}>
-          <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box margin={0}>
-                <Table size="small" style={{ width: "100%" }}>
-                  <TableBody>
-                      <TableRow className={classes.rows}>
-                        <TableCell align="center" width="1%"/>
-                        <TableCell align="center" width="25%"/>
-                        {snapCard}
-                        {pokemonCard}
-                      </TableRow>
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </StyledTableCell>
-        </TableRow>
-      </React.Fragment>
+        <React.Fragment>
+            <TableRow className={classes.rows}>
+                <TableCell align="right" width="1%">
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <StyledTableCell className={classes.headText} width="25%">{row.capability_name}</StyledTableCell>
+                <StyledTableCell >{row.snapchat}</StyledTableCell>
+                <StyledTableCell >{row.pokemongo}</StyledTableCell>
+            </TableRow>
+            <TableRow className={classes.rows}>
+                <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={0}>
+                            <Table size="small" style={{ width: "100%" }}>
+                                <TableBody>
+                                    <TableRow className={classes.rows}>
+                                        <TableCell align="center" width="1%" />
+                                        <TableCell align="center" width="25%" />
+                                        {snapCard}
+                                        {pokemonCard}
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </StyledTableCell>
+            </TableRow>
+        </React.Fragment>
     );
-  }
+}
 
 const UsecasePageContent = () => {
     const classes = makeStyles(theme => ({
         rows: {
-              '& > *': {
+            '& > *': {
                 borderBottom: 'unset',
-              },
+            },
         },
         headText: {
-          fontWeight: 'bold'
+            fontWeight: 'bold'
         },
     }))()
     return (
-    <TableContainer style={{ width: "100vw", paddingLeft: "6em" }} >
-      <Table style={{ width: 1200 }} aria-label="collapsible table">
-        <TableHead >
-          <TableRow className={classes.rows}>
-            <StyledTableCell width="1%"/>
-            <StyledTableCell className={classes.headText} width="25%">Capabilities</StyledTableCell>
-            <StyledTableCell className={classes.headText} >SnapChat</StyledTableCell>
-            <StyledTableCell className={classes.headText} >Pokmon Go</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            {data.map((row) => (
-            <Row key={row.capability_id} row={row} />
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <TableContainer style={{ width: "100vw", paddingLeft: "6em" }} >
+            <Table style={{ width: 1200 }} aria-label="collapsible table">
+                <TableHead >
+                    <TableRow className={classes.rows}>
+                        <StyledTableCell width="1%" />
+                        <StyledTableCell className={classes.headText} width="25%">Capabilities</StyledTableCell>
+                        <StyledTableCell className={classes.headText} >SnapChat</StyledTableCell>
+                        <StyledTableCell className={classes.headText} >Pokmon Go</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((row) => (
+                        <Row key={row.capability_id} row={row} />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
